164da93fbb68af0317eb240b4742e99efc041dbf
ITT <simarpreet.gujral@intimetec.com>
2016-03-03 08:02:56 +0530

    changes

--- firewall_setup.ps1
+++ firewall_setup.ps1
@@ -1,3 +1,2 @@
-netsh advfirewall firewall add rule name = WD1 dir = in protocol = tcp localport = 8172 remoteip = 90.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = WD2 dir = in protocol = tcp localport = 8172 remoteip = 98.88.65.237/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = testITT dir = in protocol = tcp localport = 3389 remoteip = 115.111.126.34/32 enable = yes action = allow
 net user WDeployAdmin pass@123456 /expires:never
