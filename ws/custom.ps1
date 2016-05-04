f94ffceaa30a1ed534b3be6c9b04c29560ee03e3
ITT <simarpreet.gujral@intimetec.com>
2016-05-04 21:53:02 +0530

    added files with prod changes

--- ws/custom.ps1
+++ ws/custom.ps1
@@ -1,23 +1,3 @@
-
-# Setting OS Level Firewall Rules
-netsh advfirewall firewall add rule name = http dir = in protocol = tcp localport = 80 enable = yes action = allow
-netsh advfirewall firewall add rule name = https dir = in protocol = tcp localport = 443 enable = yes action = allow
-netsh advfirewall firewall add rule name = boi_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 70.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = boi_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 70.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = fc_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 208.186.96.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = fc_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 208.186.96.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = boi_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 70.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = fc_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 208.186.96.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = boi_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = sac_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.102.237.130/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = boi_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 70.98.117.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = ftc_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 208.186.96.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = ftc_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 208.186.96.66/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = monitis_west1_ping dir = in remoteip = 104.200.152.54/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = monitis_west2_ping dir = in remoteip = 104.200.152.50/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = monitis_east2_ping dir = in remoteip = 173.192.200.100/32 enable = yes action = allow
-netsh advfirewall firewall add rule name = monitis_east1_ping dir = in remoteip = 173.193.219.173/32 enable = yes action = allow
-
 # Adding Web Deploy user to IIS Manager Permissions for site "hpsc-stg-web-3"
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Management")  
 [Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser("hpsc-stg-web-3-deploy", "pass@123456") 
@@ -38,6 +18,27 @@ $user.CommitChanges()
 $user = [adsi]"WinNT://$env:computername/wsadmin"
 $user.UserFlags.value = $user.UserFlags.value -bor 0x10000
 $user.CommitChanges()
+
+# Setting OS Level Firewall Rules
+netsh advfirewall firewall add rule name = http dir = in protocol = tcp localport = 80 enable = yes action = allow
+netsh advfirewall firewall add rule name = https dir = in protocol = tcp localport = 443 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = sac_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.102.237.130/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = ftc_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = ftc_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_west1_ping dir = in remoteip = 104.200.152.54/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_west2_ping dir = in remoteip = 104.200.152.50/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_east2_ping dir = in remoteip = 173.192.200.100/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_east1_ping dir = in remoteip = 173.193.219.173/32 enable = yes action = allow
+
+
 
 # Installing Visual Studio 2015 Remote Debugger
 C:\rtools_setup_x64.exe /install /quiet /norestart
