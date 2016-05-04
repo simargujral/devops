b84f99fbac30fb556670b1af89a3cd56686395ae
ITT <simarpreet.gujral@intimetec.com>
2016-05-04 15:54:02 +0530

    added files with prod changes

--- ws/custom.ps1
+++ ws/custom.ps1
@@ -1,5 +1,44 @@
-netsh advfirewall firewall add rule name = testITT dir = in protocol = tcp localport = 3389 remoteip = 115.111.126.34/32 enable = yes action = allow
-net user WDeployAdmin pass@123456 /expires:never
+
+# Setting OS Level Firewall Rules
+netsh advfirewall firewall add rule name = http dir = in protocol = tcp localport = 80 enable = yes action = allow
+netsh advfirewall firewall add rule name = https dir = in protocol = tcp localport = 443 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug1 dir = in protocol = tcp localport = 4020 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug2 dir = in protocol = tcp localport = 4021 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = fc_VS2015_remote_debug_disc dir = in protocol = udp localport = 3702 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = sac_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 70.102.237.130/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = boi_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 70.98.117.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = ftc_office_deploy dir = in protocol = tcp localport = 8172 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = ftc_office_rdp dir = in protocol = tcp localport = 3389 remoteip = 208.186.96.66/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_west1_ping dir = in remoteip = 104.200.152.54/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_west2_ping dir = in remoteip = 104.200.152.50/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_east2_ping dir = in remoteip = 173.192.200.100/32 enable = yes action = allow
+netsh advfirewall firewall add rule name = monitis_east1_ping dir = in remoteip = 173.193.219.173/32 enable = yes action = allow
+
+# Adding Web Deploy user to IIS Manager Permissions for site "hpsc-stg-web-3"
 [System.Reflection.Assembly]::LoadWithPartialName(“Microsoft.Web.Management”)  
-[Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser("MyUser", "pass@123456") 
-[Microsoft.Web.Management.Server.ManagementAuthorization]::Grant("MyUser", "hpsc-stg-web-3", $FALSE)
+[Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser("hpsc-stg-web-3-deploy", "pass@123456") 
+[Microsoft.Web.Management.Server.ManagementAuthorization]::Grant("hpsc-stg-web-3-deploy", "hpsc-stg-web-3", $FALSE)
+
+# Setting password for WDeployAdmin
+net user WDeployAdmin pass@123456 /expires:never
+
+# Setting password for WDeployAdmin, WDeployConfigWriter and wsadmin to expire never
+$user = [adsi]"WinNT://$env:computername/WDeployAdmin"
+$user.UserFlags.value = $user.UserFlags.value -bor 0x10000
+$user.CommitChanges()
+
+$user = [adsi]"WinNT://$env:computername/WDeployConfigWriter"
+$user.UserFlags.value = $user.UserFlags.value -bor 0x10000
+$user.CommitChanges()
+
+$user = [adsi]"WinNT://$env:computername/wsadmin"
+$user.UserFlags.value = $user.UserFlags.value -bor 0x10000
+$user.CommitChanges()
+
+# Installing Visual Studio 2015 Remote Debugger
+C:\rtools_setup_x64.exe /install /quiet /norestart
+sleep 150
--- ws/hpscstgweb3.ps1
+++ ws/hpscstgweb3.ps1
@@ -1,54 +1,93 @@
 Configuration hpsc_stg_web3
 {
-    param
-    (
-        [string]$WebPiSourcePath="C:\Program Files\WindowsPowerShell\Modules\xWebAdministration\WebPi\wpilauncher.exe",
-        [string]$WebPiCmdPath ="$env:ProgramFiles\Microsoft\Web Platform Installer\WebPiCmd-x64.exe"
-    )
 	Import-DscResource -ModuleName xWebAdministration
+	Import-DSCResource -ModuleName xPSDesiredStateConfiguration
 
-    Node 'localhost'
-    {    
+    Node ('localhost')
+    {   
+        # Install IIS features	
         WindowsFeature IIS 
         { 
-            Ensure          = "Present" 
-            Name            = "Web-Server" 
+            Ensure          = "Present"
+            Name            = "Web-Server"			
         } 
-        # Install the ASP .NET 4.6 role 
-        WindowsFeature AspNet46 
+        # Install the ASP .NET 4.5 role 
+        WindowsFeature AspNet45 
         { 
             Ensure          = "Present" 
-            Name            = "Web-Asp-Net45" 
+            Name            = "Web-Asp-Net45"
+			DependsOn       = '[WindowsFeature]IIS'
         }
+		# Enable Web Management Console
         WindowsFeature WebServerManagementConsole
         {
             Name = "Web-Mgmt-Console"
             Ensure = "Present"
         }
+		# Enable Web Management Service
         WindowsFeature Web-Mgmt-Service  
         {  
             Ensure          = 'Present'  
             Name            = 'Web-Mgmt-Service' 	  
             DependsOn       = '[WindowsFeature]IIS'  
         }
-        Package WebPi_Installation
+        # Download Web Platform Installer exe from web
+		xRemoteFile wpidownload
+		{
+			DestinationPath = "C:\wpilauncher.exe"
+            Uri = "http://download.microsoft.com/download/F/4/2/F42AB12D-C935-4E65-9D98-4E56F9ACBC8E/wpilauncher.exe"
+			DependsOn       = '[WindowsFeature]IIS'
+		}
+		# Download Visual Studio 2015 Remote Debugger exe from web
+        xRemoteFile vsremotedebugger
+        {
+            DestinationPath = "C:\rtools_setup_x64.exe"
+            Uri = "https://download.microsoft.com/download/E/7/A/E7AEA696-A4EB-48DD-BA4A-9BE41A402400/rtools_setup_x64.exe"
+			DependsOn       = '[WindowsFeature]IIS'
+            
+        }
+		<#Package WebPi_Installation
         {
             Ensure = "Present"
             Name = "Microsoft Web Platform Installer 5.0"
             Path = $WebPiSourcePath
             ProductId = '4D84C195-86F0-4B34-8FDE-4A17EB41306A'
             Arguments = ''
+        }#>
+		# Installing Web Platform Installer
+		Package wpiinstall
+        {
+            Ensure = "Present"
+            Name = "Microsoft Web Platform Installer 5.0"
+            Path = "C:\wpilauncher.exe"
+            ProductId = "4D84C195-86F0-4B34-8FDE-4A17EB41306A"
+			DependsOn = '[xRemoteFile]wpidownload'
+            
         }
-
+		# Installing Web Deploy
         Package WebDeploy_Installation
         {
             Ensure = "Present"
-            Name = "Microsoft Web Deploy 3.6"
-            Path = $WebPiCmdPath
+            Name = "Microsoft Web Deploy 3.5"
+            Path = "$env:ProgramFiles\Microsoft\Web Platform Installer\WebPiCmd-x64.exe"
             ProductId = ''
             Arguments = "/install /products:WDeploy /AcceptEula"
             DependsOn = @("[Package]WebPi_Installation")
         }
+		<#
+		Package rtools
+        {
+            Ensure = "Present"
+            Name = "Microsoft Visual Studio 2015 Remote Debugger"
+            Path = "C:\rtools_setup_x64.exe"
+			Arguments = "/norestart"
+            ProductId = ""
+			LogPath = "C:\rtools.log"
+			DependsOn       = '[xRemoteFile]vsremotedebugger'
+			
+            
+        }#>
+        # Stopping Default Website
 	    xWebsite DefaultSite  
         { 
             Ensure          = "Present" 
@@ -57,7 +96,7 @@ Configuration hpsc_stg_web3
             PhysicalPath    = "C:\inetpub\wwwroot" 
             DependsOn       = "[WindowsFeature]IIS" 
         }
-    # enable remote deployments
+        # enable remote deployments
         Registry Enable-Remote-Management {
             Ensure         = 'Present'
             Key            = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'  
@@ -66,41 +105,14 @@ Configuration hpsc_stg_web3
             ValueType      = 'Dword'
             DependsOn      = '[WindowsFeature]Web-Mgmt-Service'  
         }
-        <# Start msdeploy service is running
-        Service WMSvc-Running
-        {
-            Name        = "WMSvc"
-            StartupType = "Automatic"
-            State       = "Running"
-            DependsOn   = '[Registry]Enable-Remote-Management'  
-        }
-        xWaitForService WMSvc-Wait-Running
-        {
-            Name         = 'WMSvc'
-            State        = 'Running'
-            Restart      = $true
-            DependsOn    = '[Service]WMSvc-Running'  
-        }#>
-        #
         # Create the Website 
-        #
-        File hpsc-stg-web-3 {
+        <#File hpsc-stg-web-3 {
             Type            = 'Directory'
             DestinationPath = 'C:\inetpub\wwwroot\hpsc-stg-web-3'
             Ensure          = "Present"
             DependsOn       = '[WindowsFeature]IIS'  
-        }        
-        # Copy the website content 
-        #File WebContent 
-        #{ 
-        #    Ensure          = "Present" 
-        #    SourcePath      = "C:\Program Files\WindowsPowerShell\Modules\xWebAdministration\mysite" 
-        #    DestinationPath = "C:\inetpub\wwwroot\hpsc-stg-web-3"
-        #    Recurse         = $true 
-        #    Type            = "Directory" 
-          #  DependsOn       = "[WindowsFeature]AspNet45" 
-        #}        
-        # Create the new Website with HTTPS 
+        }  #>              
+        # Create the new Website
         xWebsite hpsc-stg-web-3 
         { 
             Ensure          = "Present" 
@@ -112,13 +124,13 @@ Configuration hpsc_stg_web3
                               {
                                   Protocol              = 'http'
                                   Port                  = '80'
-                                  HostName              = ''
+                                  HostName              = 'hspc-stg-web-3.hpsalescentral.com'
                               }
                               MSFT_xWebBindingInformation
                               {
                                   Protocol              = 'https'
                                   Port                  = '443'
-                                  HostName              = ''
+                                  HostName              = 'hspc-stg-web-3.hpsalescentral.com'
                               } )
         }
     }
