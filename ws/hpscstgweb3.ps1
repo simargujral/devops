01147b4cd2498127412fe505202d77c8e221fc56
ITT <simarpreet.gujral@intimetec.com>
2016-05-10 14:06:25 +0530

    changed certificate script resource

--- ws/hpscstgweb3.ps1
+++ ws/hpscstgweb3.ps1
@@ -87,7 +87,7 @@ Configuration hpsc_stg_web3
 			
             
         }#>
-		Script Install-VSRemoteDebugger
+		Script InstallVSRemoteDebugger
         {
             SetScript = {
                 Write-Verbose "Starting Installation of VS2015 Remote Debugger"			
@@ -186,10 +186,7 @@ Configuration hpsc_stg_web3
                 if($getuser.enabled -eq "True"){
                     return $true
 			    }
-				else{
 				    return $false
-				}
-
             }
             GetScript = { 
                 return @{
@@ -206,7 +203,7 @@ Configuration hpsc_stg_web3
                 Write-Verbose "Installing SSL cert"
                 Import-Certificate -FilePath "C:\Program Files\WindowsPowerShell\Modules\xWebAdministration\hp-idp.cert.cer" -CertStoreLocation "Cert:\LocalMachine\my"
                 $certpwd = ConvertTo-SecureString -String "W1rest0ne!" -Force -AsPlainText
-                Import-PfxCertificate -FilePath "C:\Program Files\WindowsPowerShell\Modules\xWebAdministration\wildcard_hpsalescentral_com.pfx" -CertStoreLocation "Cert:\LocalMachine\my" -Password $certpwd				
+                Import-PfxCertificate –FilePath "C:\Program Files\WindowsPowerShell\Modules\xWebAdministration\wildcard_hpsalescentral_com.pfx" -CertStoreLocation "Cert:\LocalMachine\my" -Password $certpwd				
             }
             TestScript = {
                     $check_cert=Get-ChildItem Cert:\LocalMachine\my
